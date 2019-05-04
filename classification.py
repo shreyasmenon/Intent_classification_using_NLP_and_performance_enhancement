6b753a93adfcb1e631c0fcfd48793becdab712bb
Shreyas Menon <menonshreyas14@gmail.com>
2019-05-04 18:36:20 -0400

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,8 +1,15 @@
+from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split , StratifiedKFold
 
 
 X_train, X_test, y_train, y_test = train_test_split(df['query_request'], df['intent'], test_size=0.2, random_state=0)
+
+#Guassian NB
+nb_model = GaussianNB()
+model.fit(X_train,y_train)
+predicted = model.predict(X_test)
+print(classification_report(y_test,predicted))
 
 #Support vector machines
 svc_model = SVC(gamma='auto')
