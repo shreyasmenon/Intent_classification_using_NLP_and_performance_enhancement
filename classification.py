29551ba1152763849be3450b5cd29cf4213a1df7
Shreyas Menon <menonshreyas14@gmail.com>
2019-05-04 19:12:38 -0400

    Update classification.py

--- classification.py
+++ classification.py
@@ -17,4 +17,39 @@ svc_model.fit(X_train, y_train)
 predicted= svc_model.predict(X_test)
 print(classification_report(y_test,predicted))
 
+#Neural Networks
+encoder = LabelEncoder()
+topic_labels = encoder.fit_transform(df['intent'])
+
+encoder = OneHotEncoder(sparse=False)
+topic_labels = topic_labels.reshape((topic_labels.shape[0] , 1))  
+oht_encoder = encoder.fit_transform(topic_labels)
+
+NN_X_train, NN_X_test, NN_y_train, NN_y_test = train_test_split(df['query'], oht_encoder ,  test_size=0.2, random_state=0)
+
+#word embedding
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(NN_X_train)
+NN_X_train = tokenizer.texts_to_sequences(NN_X_train)
+NN_X_test = tokenizer.texts_to_sequences(NN_X_test)
+vocab_size = len(tokenizer.word_index) + 1
+
+#padding
+maxlen = 100
+NN_X_train  = pad_sequences(NN_X_train, padding='post', maxlen=maxlen)
+NN_X_test = pad_sequences(NN_X_test, padding='post', maxlen=maxlen)
+
+embedding_dim = 50
+input_dim =  vocab_size 
+
+nn_model = Sequential()
+
+nn_model.add(layers.Embedding(input_dim=input_dim, output_dim=embedding_dim, input_length=maxlen))
+nn_model.add(layers.GlobalMaxPool1D())
+nn_model.add(layers.Dense(512, activation='relu'))
+nn_model.add(layers.Dense(9, activation='sigmoid'))
+#nn_model.compile(loss='categorical_crossentropy', optimizer = optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False),  metrics=['accuracy'])   
+nn_model.compile(loss='categorical_crossentropy', optimizer = optimizers.Adadelta(lr=0.5, rho=0.95, epsilon=None, decay=0.0),  metrics=['accuracy'])   
+nn_model.summary()
+
 
